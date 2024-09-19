52b0a46d974b727dbfc1f336eacde0d5dc7681a6
Sidharth Malpani <135989098+areysid@users.noreply.github.com>
2024-09-19 16:14:58 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,17 @@
-#!/bin/bash
+sudo apt update
 
-# TODO your deploy script implementation...
+cd ./NetflixMovieCatalog
+
+source .venv/bin/activate
+
+cd ..
+
+sudo systemctl stop catalog.service
+
+sudo systemctl enable catalog.service
+
+sudo systemctl start catalog.service
+
+sudo systemctl start nginx
+
+sudo apt update
